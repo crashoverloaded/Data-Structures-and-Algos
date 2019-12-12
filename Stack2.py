07977abef1e963077c2e029f42f5964e211f2df2
Priyank Agarwal <priyankagarwal00.pa@gmail.com>
2019-12-12 13:34:41 +0530

    Adding Is_Empty Function

---
+++ Stack2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class Stack():
+	def __init__(self):
+		self.items=[]
+	def push(self,item):
+		self.items.append(item)
+	def pop(self):
+		return self.items.pop()
+	def is_empty(self):
+		return self.items == []
+	def get_stack(self):
+		return self.items
+
+s = Stack()
+print(s.is_empty())
+s.push("A")
+print(s.is_empty())
